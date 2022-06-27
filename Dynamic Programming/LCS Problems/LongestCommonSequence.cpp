c9cc3aee49d1e17bba9d9ce886bac1ea70d6a086
Vishal pandey <60701275+vishalpa8@users.noreply.github.com>
2022-06-27 13:50:23 +0530

    Updated

--- Dynamic Programming/LCS Problems/LongestCommonSequence.cpp
+++ Dynamic Programming/LCS Problems/LongestCommonSequence.cpp
@@ -35,6 +35,24 @@ int lcswithmemoize(int x, int y, string s1, string s2){
 	return dp[x][y];
 }
 
+int lcsTopDownApproach(string s1,string s2, int x, int y){
+
+	for(int i=1; i<x+1; i++){
+		for(int j=1; j<y+1; j++){
+			if(s1[i-1] == s2[j-1]){
+				dp[i][j] = 1 + dp[i-1][j-1]; 
+			}
+			else{
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+			}
+		}
+	}
+
+	return dp[x][y];
+
+}
+
+
 
 int main()
 {
@@ -43,6 +61,8 @@ int main()
 	
 	cout << lcswithoutmemoize(6,6,"vishal","pandey") << endl;
 	cout << lcswithmemoize(6,6,"vishal","pandey") << endl;
+	cout << lcsTopDownApproach("acbcf","abcdaf",5,6) << endl;
+
 
 	return 0;
 
