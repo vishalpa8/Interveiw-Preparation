625c5051d5f51cd105191a37086c92fbe18e6d20
Vishal pandey <60701275+vishalpa8@users.noreply.github.com>
2022-06-27 14:09:40 +0530

    Updated

--- Dynamic Programming/LCS Problems/LongestCommonSequence.cpp
+++ Dynamic Programming/LCS Problems/LongestCommonSequence.cpp
@@ -35,21 +35,19 @@ int lcswithmemoize(int x, int y, string s1, string s2){
 	return dp[x][y];
 }
 int lcsTopDownApproach(string s1,string s2, int x, int y){
-	int t[x+1][y+1];
-	memset(t,0,sizeof(t));
 
 	for(int i=1; i<x+1; i++){
 		for(int j=1; j<y+1; j++){
 			if(s1[i-1] == s2[j-1]){
-				t[i][j] = 1 + t[i-1][j-1]; 
+				dp[i][j] = 1 + dp[i-1][j-1]; 
 			}
 			else{
-				t[i][j] = max(t[i-1][j],t[i][j-1]);
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
 			}
 		}
 	}
 
-	return t[x][y];
+	return dp[x][y];
 
 }
 
@@ -57,7 +55,7 @@ int lcsTopDownApproach(string s1,string s2, int x, int y){
 int main()
 {
 	fast_io;
-	memset(dp,-1,sizeof(dp));
+	memset(dp,0,sizeof(dp));
 	
 	cout << lcswithoutmemoize(6,6,"vishal","pandey") << endl;
 	cout << lcswithmemoize(6,6,"vishal","pandey") << endl;
