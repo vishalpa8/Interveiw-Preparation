58471f0ba0920258252c3f3f37a661083823ed37
Vishal pandey <60701275+vishalpa8@users.noreply.github.com>
2022-07-02 22:54:27 +0530

    Updated

--- Array/Intersection.cpp
+++ Array/Intersection.cpp
@@ -9,24 +9,23 @@ vector<int>commonElements (int A[], int B[], int C[], int n1, int n2, int n3){
     vector<int> v;
       while(i<n1 && j<n2 && k<n3){
             if(i<n1 && A[i] == A[i+1])
-             i++;
+              i++;
 			if(j<n2 && B[j] == B[j+1])
- 				j++;
+			 	j++;
 			if(k<n3 && C[k] == C[k+1])
- 				k++;
+				k++;
 			else if((A[i] == B[j]) && (B[j] == C[k])){
- 				v.push_back(A[i]);
-  				i++;j++;k++;
-			}
+					v.push_back(A[i]);
+					i++;j++;k++;
+				}
 			else{
-   			int m=max({A[i],B[j],C[k]});
-			if(A[i]<m)
-				i++;
-			if(B[j]<m)
- 				j++;
-
-			if(C[k]<m)
- 				k++;
+					int m=max({A[i],B[j],C[k]});
+					if(A[i]<m)
+							i++;
+					if(B[j]<m)
+							j++;
+					if(C[k]<m)
+							k++;
 			}
 	  }
 	  return v;
